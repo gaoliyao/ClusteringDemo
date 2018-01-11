dbe8520ee50134c188627716e049537e95b33564
Liyao Gao <gao463@purdue.edu>
2018-01-11 12:18:31 -0500

    Kmeans added

---
+++ KMeans.py
@@ -0,0 +1,8 @@
+from sklearn.cluster import KMeans
+import numpy as np
+X = np.array([[1, 2], [1, 4], [1, 0], [4, 2], [4, 4], [4, 0]])
+kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
+print(X)
+print(kmeans.labels_)
+print(kmeans.predict([[0, 0], [4, 4]]))
+print(kmeans.cluster_centers_)
